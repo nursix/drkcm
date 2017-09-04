c8ff2e394c145aa4ed99853b3ac2d87d27afcc34
Dominic König <dominic@nursix.org>
2017-09-04 13:04:45 +0200

    Release DRKCM-0.9.1-beta

---
+++ modules/templates/DRKCM/upgrade/0.9.0-0.9.1.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#
+# Database upgrade script
+#
+# DRKCM Template Version 0.9.0 => 0.9.1
+#
+# Execute in web2py folder after code upgrade like:
+# python web2py.py -S eden -M -R applications/eden/modules/templates/DRKCM/upgrade/0.9.0-0.9.1.py
+#
+#import datetime
+import sys
+#from s3 import S3DateTime
+
+#from gluon.storage import Storage
+#from gluon.tools import callback
+
+# Override auth (disables all permission checks)
+auth.override = True
+
+# Failed-flag
+failed = False
+
+# Info
+def info(msg):
+    print >> sys.stderr, msg,
+def infoln(msg):
+    print >> sys.stderr, msg
+
+# Load models for tables
+#ctable = s3db.dvr_case
+
+IMPORT_XSLT_FOLDER = os.path.join(request.folder, "static", "formats", "s3csv")
+TEMPLATE_FOLDER = os.path.join(request.folder, "modules", "templates", "DRKCM")
+
+# -----------------------------------------------------------------------------
+# Upgrade user roles
+if not failed:
+    info("Upgrade user roles")
+
+    bi = s3base.S3BulkImporter()
+    filename = os.path.join(TEMPLATE_FOLDER, "auth_roles.csv")
+
+    with open(filename, "r") as File:
+        try:
+            bi.import_role(filename)
+        except Exception, e:
+            infoln("...failed")
+            infoln(sys.exc_info()[1])
+            failed = True
+        else:
+            infoln("...done")
+
+# -----------------------------------------------------------------------------
+# Finishing up
+#
+if failed:
+    db.rollback()
+    print >> sys.stderr, "UPGRADE FAILED - Action rolled back."
+else:
+    db.commit()
+    print >> sys.stderr, "UPGRADE SUCCESSFUL."
