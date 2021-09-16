37f1d4445acb48f04fe5d3e85ec3a9ce5ccc1fd5
Dominic König <dominic@nursix.org>
2021-09-16 23:33:13 +0200

    Remove debug

--- modules/core/io/codec.py
+++ modules/core/io/codec.py
@@ -68,7 +68,6 @@ class S3Codec(object):
         name = cls.CODECS.get(fmt)
         if name:
             package = "core.io.codecs.%s" % fmt
-            print(package)
             try:
                 codec = getattr(__import__(package, fromlist=[name]), name)
             except (ImportError, AttributeError):
